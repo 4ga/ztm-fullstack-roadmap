1b894f850d0d7bf940bb05ac441e7007c440efa5
Grace <67880424+akpg02@users.noreply.github.com>
2026-03-08 22:28:48 -0600

    feat: were config into server bootstrap (fail-fast)

--- packages/phase1/p03/src/server.ts
+++ packages/phase1/p03/src/server.ts
@@ -1,5 +1,5 @@
 import { app } from "./app";
-import { config } from "../src/config";
+import { config } from "./config";
 
 app.listen(config.port, () =>
   console.log(`Server listening on port ${config.port}`),
